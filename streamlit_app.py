b9f270fdc06136091c204fda234ffcfe85a4dd49
Octavi Semonin <octavi@gmail.com>
2024-09-27 21:58:22 +0000

    now loads and processes, but placeholder file

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,53 +1,205 @@
 import streamlit as st
-from openai import OpenAI
+
+import cv2
+import pickle
+import numpy as np
+import pandas as pd
+
+from datetime import timedelta
+from google.cloud import videointelligence
+from google.oauth2 import service_account
+
+import ffmpy
 
 # Show title and description.
-st.title("📄 Document question answering")
+st.title("🚸 Street Counting")
 st.write(
-    "Upload a document below and ask a question about it – GPT will answer! "
-    "To use this app, you need to provide an OpenAI API key, which you can get [here](https://platform.openai.com/account/api-keys). "
+    "Upload a video below and the robots will count stuff! "
+    # "To use this app, you need to provide an OpenAI API key, which you can get [here](https://platform.openai.com/account/api-keys). "
 )
 
-# Ask user for their OpenAI API key via `st.text_input`.
-# Alternatively, you can store the API key in `./.streamlit/secrets.toml` and access it
-# via `st.secrets`, see https://docs.streamlit.io/develop/concepts/connections/secrets-management
-openai_api_key = st.text_input("OpenAI API Key", type="password")
-if not openai_api_key:
-    st.info("Please add your OpenAI API key to continue.", icon="🗝️")
-else:
+# # Ask user for their OpenAI API key via `st.text_input`.
+# # Alternatively, you can store the API key in `./.streamlit/secrets.toml` and access it
+# # via `st.secrets`, see https://docs.streamlit.io/develop/concepts/connections/secrets-management
+# gcp_api_key = st.text_input("Google Cloud API Key", type="password")
+# if not gcp_api_key:
+#     st.info("Please add your Google Cloud API key to continue.", icon="🗝️")
+# else:
 
-    # Create an OpenAI client.
-    client = OpenAI(api_key=openai_api_key)
+# Create a GCP service account.
+# client = OpenAI(api_key=openai_api_key)
+credentials = service_account.Credentials.from_service_account_info(
+    st.secrets["gcp_service_account"]
+)
+video_client = videointelligence.VideoIntelligenceServiceClient(credentials=credentials)
 
-    # Let the user upload a file via `st.file_uploader`.
-    uploaded_file = st.file_uploader(
-        "Upload a document (.txt or .md)", type=("txt", "md")
+# Let the user upload a file via `st.file_uploader`.
+video = st.file_uploader(
+    "Upload a document (.mp4, .mov, or .avi)", type=("mp4", "mov", "avi")
+)
+
+# Send to Google
+features = [videointelligence.Feature.OBJECT_TRACKING]
+
+if video:
+    # operation = video_client.annotate_video(
+    #     request={"features": features, 
+    #                 "input_content": video.getvalue()}# "input_content": input_content}
+    # )
+
+    st.write("\nProcessing video for object annotations (hang in there, this takes awhile).")
+
+    # result = operation.result(timeout=500)
+    st.write("\nFinished processing.\n")
+
+    temp = 'result-4a2e7a146c8f7bc1dd61acac940c5b04.pickle'
+    with open(temp, 'rb') as handle:
+        result = pickle.load(handle)
+
+    # Analyze
+    object_annotations = result.annotation_results[0].object_annotations
+    object_annotation = object_annotations[1]
+    objects = []
+    tracks = []
+
+    for n,obj in enumerate(object_annotations):
+        
+        objects.append(obj.entity.description)
+
+        for f in obj.frames:
+
+            track = [
+                n,
+                obj.entity.description,
+                f.time_offset.total_seconds(),
+                f.normalized_bounding_box.left,
+                f.normalized_bounding_box.right,
+                f.normalized_bounding_box.top,
+                f.normalized_bounding_box.bottom,
+            ]
+            tracks.append(track) 
+
+    objects = pd.Series(objects, name='type')
+    tracks = pd.DataFrame(tracks, columns=['n','description','time','left','right','top','bottom'])
+
+    tracks['on_left'] = tracks['right'] < 0.5
+    tracks['on_right'] = tracks['left'] > 0.5
+    first_left = tracks.loc[tracks['on_left']].groupby('n').first()['time']
+    first_left = first_left.rename('first_left')
+    first_right = tracks.loc[tracks['on_right']].groupby('n').first()['time']
+    first_right = first_right.rename('first_right')
+
+    objects = pd.concat([objects, first_left, first_right], axis=1)
+
+    objects['crossing'] = None
+    objects.loc[objects['first_left'] < objects['first_right'], 'crossing'] = 'right'
+    objects.loc[objects['first_left'] > objects['first_right'], 'crossing'] = 'left'
+
+    st.write(f"{len(objects)} objects found:")
+    st.dataframe(objects)
+
+    n_crossing_right = sum(objects['crossing']=='right')
+    n_crossing_left = sum(objects['crossing']=='left')
+    counts = objects.groupby(['type','crossing']).size().unstack()
+    st.write(f"{n_crossing_right} crossing right, {n_crossing_left} crossing left:")
+    st.dataframe(counts)
+
+    st.download_button(
+        "Download objects",
+        objects.to_csv().encode('utf-8'),
+        "objects.csv",
+        "text/csv",
     )
 
-    # Ask the user for a question via `st.text_area`.
-    question = st.text_area(
-        "Now ask a question about the document!",
-        placeholder="Can you give me a short summary?",
-        disabled=not uploaded_file,
+    st.download_button(
+        "Download tracks",
+        tracks.to_csv().encode('utf-8'),
+        "tracks.csv",
+        "text/csv",
     )
 
-    if uploaded_file and question:
+    ffmpy
+    # st.video(video)
 
-        # Process the uploaded file and question.
-        document = uploaded_file.read().decode()
-        messages = [
-            {
-                "role": "user",
-                "content": f"Here's a document: {document} \n\n---\n\n {question}",
-            }
-        ]
+    # Draw annotations
+    colors = {
+        'car': (0,0,255),
+        'person': (255,165,0), 
+        'bicycle': (255,0,0),
+        'toy vehicle': (250,128,114),
+        'lighting': (255,255,0), 
+        'tire': (0,0,0), 
+        'luggage & bags': (150,75,0), 
+        'building': (128,128,128), 
+        'shoe': (128,128,128), 
+        'mirror': (127, 0, 0),
+        'wheel': (0, 127, 0)
+    }
 
-        # Generate an answer using the OpenAI API.
-        stream = client.chat.completions.create(
-            model="gpt-3.5-turbo",
-            messages=messages,
-            stream=True,
-        )
+    with open(video.name, mode='wb') as f:
+        f.write(video.read()) # save video to disk
 
-        # Stream the response to the app using `st.write_stream`.
-        st.write_stream(stream)
+    cap = cv2.VideoCapture(video.name)
+    length = cap.get(cv2.CAP_PROP_FRAME_COUNT)
+    fps = cap.get(cv2.CAP_PROP_FPS)
+    timestamps = []
+    frame_number = 0
+
+    output_path = f'{video.name}-annotated.mp4'
+    fourcc = cv2.VideoWriter_fourcc(*'h264')
+    width = int(cap.get(3)) 
+    height = int(cap.get(4)) 
+
+    out = cv2.VideoWriter(output_path, fourcc, fps, (width,  height))
+
+    progress_text = "Drawing annotations. Please wait."
+    my_bar = st.progress(0, text=progress_text)
+
+    while(cap.isOpened()):
+        ret, img = cap.read()
+
+        if ret:
+            for n,obj in enumerate(object_annotations):
+                description = obj.entity.description
+                entity_id = obj.entity.entity_id
+                
+                for f in obj.frames:
+                    obj_frame = round(f.time_offset.total_seconds() * fps)
+                    
+                    if abs(frame_number-obj_frame) < 2:
+                        left = int(f.normalized_bounding_box.left * img.shape[1])
+                        right = int(f.normalized_bounding_box.right * img.shape[1])
+                        top = int(f.normalized_bounding_box.top * img.shape[0])
+                        bottom = int(f.normalized_bounding_box.bottom * img.shape[0])
+        
+                        if description in colors.keys():
+                            color = colors[description]
+                        else: 
+                            color = (255,255,255)
+                            
+                        cv2.rectangle(img, (left, top), (right, bottom), 
+                                    color, 2)
+                        cv2.putText(img, f'Obj #{n}, {description}', (left, bottom),
+                                    cv2.FONT_HERSHEY_SIMPLEX, 1, 
+                                    color, 2)
+        
+            timestamps.append(cap.get(cv2.CAP_PROP_POS_MSEC))
+            frame_number += 1
+
+            out.write(img)
+
+            my_bar.progress(frame_number/length, text=progress_text)
+            # cv2.imshow("annotations", img)
+            # if cv2.waitKey(1) == 27:
+            #     break
+        else:
+            break
+
+    cap.release()
+    out.release()
+    my_bar.empty()
+
+    with open(output_path, 'rb') as f:
+        st.download_button('Download annotated video', f, file_name=output_path)  # Defaults to 'application/octet-stream'
+
+    st.video(output_path)
