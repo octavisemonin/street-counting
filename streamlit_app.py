3d5b4bb64603f6965ff36566a41992637a52156a
Octavi Semonin <octavi@gmail.com>
2024-09-30 19:17:16 +0000

    reorganize, add heatmap

--- streamlit_app.py
+++ streamlit_app.py
@@ -13,6 +13,81 @@ from google.oauth2 import service_account
 
 from ffmpy import FFmpeg
 
+colors = {
+    'car': (0,0,255),
+    'person': (0,255,0), 
+    'bicycle': (255,0,0),
+    # 'toy vehicle': (250,128,114),
+    # 'lighting': (255,255,0), 
+    # 'tire': (0,0,0), 
+    # 'luggage & bags': (150,75,0), 
+    # 'building': (128,128,128), 
+    # 'shoe': (128,128,128), 
+    # 'mirror': (127, 0, 0),
+    # 'wheel': (0, 127, 0)
+}
+
+def analyze(result):
+    """Analyze the objects and tracks from Google"""
+
+    object_annotations = result.annotation_results[0].object_annotations
+    objects = []
+    tracks = []
+
+    for n,obj in enumerate(object_annotations):
+        
+        objects.append(obj.entity.description)
+
+        for f in obj.frames:
+
+            track = [
+                n,
+                obj.entity.description,
+                f.time_offset.total_seconds(),
+                f.normalized_bounding_box.left,
+                f.normalized_bounding_box.right,
+                f.normalized_bounding_box.top,
+                f.normalized_bounding_box.bottom,
+            ]
+            tracks.append(track) 
+
+    objects = pd.Series(objects, name='type')
+    tracks = pd.DataFrame(tracks, columns=['n','description','time','left','right','top','bottom'])
+
+    tracks['on_left'] = tracks['right'] < 0.5
+    tracks['on_right'] = tracks['left'] > 0.5
+    first_left = tracks.loc[tracks['on_left']].groupby('n').first()['time']
+    first_left = first_left.rename('first_left')
+    first_right = tracks.loc[tracks['on_right']].groupby('n').first()['time']
+    first_right = first_right.rename('first_right')
+
+    objects = pd.concat([objects, first_left, first_right], axis=1)
+
+    objects['crossing'] = None
+    objects.loc[objects['first_left'] < objects['first_right'], 'crossing'] = 'right'
+    objects.loc[objects['first_left'] > objects['first_right'], 'crossing'] = 'left'
+
+    n_crossing_right = sum(objects['crossing']=='right')
+    n_crossing_left = sum(objects['crossing']=='left')
+    counts = objects.groupby(['type','crossing']).size().unstack()
+
+    # Log run history in Google Sheets
+    df = sheets_client.read(ttl=0)
+    df.loc[len(df)] = [datetime.now(), video.name, video.size, 
+                         tracks['time'].max(), process_time,
+                         len(objects), len(tracks)]
+    sheets_client.update(worksheet=0, data=df)
+
+    # Return
+    r = {
+        'objects':objects, 'tracks':tracks,
+        'n_crossing_right':n_crossing_right,
+        'n_crossing_left':n_crossing_left,
+        'counts':counts,
+    }
+
+    return r
+
 # Session state initialization
 if 'annotation_result' not in st.session_state:
     st.session_state['annotation_result'] = None
@@ -50,67 +125,22 @@ if video and st.session_state['annotation_result'] is None:
 
         # Use this for debugging instead of using API calls:
         # temp = 'result-4a2e7a146c8f7bc1dd61acac940c5b04.pickle'
+        # temp = 'result-96b3339212e7b4771f6c002cc97cff78.pickle'
         # with open(temp, 'rb') as handle:
         #     result = pickle.load(handle)
 
         st.session_state['annotation_result'] = result
         process_time = time.time() - start_time
-        st.write("\nFinished processing.\n")
+        st.write("Finished processing.")
 
     # Analyze
-    object_annotations = result.annotation_results[0].object_annotations
-    object_annotation = object_annotations[1]
-    objects = []
-    tracks = []
-
-    for n,obj in enumerate(object_annotations):
-        
-        objects.append(obj.entity.description)
-
-        for f in obj.frames:
-
-            track = [
-                n,
-                obj.entity.description,
-                f.time_offset.total_seconds(),
-                f.normalized_bounding_box.left,
-                f.normalized_bounding_box.right,
-                f.normalized_bounding_box.top,
-                f.normalized_bounding_box.bottom,
-            ]
-            tracks.append(track) 
-
-    objects = pd.Series(objects, name='type')
-    tracks = pd.DataFrame(tracks, columns=['n','description','time','left','right','top','bottom'])
-
-    tracks['on_left'] = tracks['right'] < 0.5
-    tracks['on_right'] = tracks['left'] > 0.5
-    first_left = tracks.loc[tracks['on_left']].groupby('n').first()['time']
-    first_left = first_left.rename('first_left')
-    first_right = tracks.loc[tracks['on_right']].groupby('n').first()['time']
-    first_right = first_right.rename('first_right')
-
-    objects = pd.concat([objects, first_left, first_right], axis=1)
-
-    objects['crossing'] = None
-    objects.loc[objects['first_left'] < objects['first_right'], 'crossing'] = 'right'
-    objects.loc[objects['first_left'] > objects['first_right'], 'crossing'] = 'left'
-
+    r = analyze(result)
+    objects = r['objects']
+    tracks = r['tracks']
     st.write(f"{len(objects)} objects found:")
     st.dataframe(objects)
-
-    n_crossing_right = sum(objects['crossing']=='right')
-    n_crossing_left = sum(objects['crossing']=='left')
-    counts = objects.groupby(['type','crossing']).size().unstack()
-    st.write(f"{n_crossing_right} crossing right, {n_crossing_left} crossing left:")
-    st.dataframe(counts)
-
-    # Log run history in Google Sheets
-    df = sheets_client.read(ttl=0)
-    df.loc[len(df)] = [datetime.now(), video.name, video.size, 
-                         tracks['time'].max(), process_time,
-                         len(objects), len(tracks)]
-    sheets_client.update(worksheet=0, data=df)
+    st.write(f"{r['n_crossing_right']} crossing right, {r['n_crossing_left']} crossing left:")
+    st.dataframe(r['counts'])
 
     # User can download their data
     st.download_button(
@@ -127,23 +157,8 @@ if video and st.session_state['annotation_result'] is None:
         "text/csv",
     )
 
-    # Draw annotations
-    colors = {
-        'car': (0,0,255),
-        'person': (255,165,0), 
-        'bicycle': (255,0,0),
-        'toy vehicle': (250,128,114),
-        'lighting': (255,255,0), 
-        'tire': (0,0,0), 
-        'luggage & bags': (150,75,0), 
-        'building': (128,128,128), 
-        'shoe': (128,128,128), 
-        'mirror': (127, 0, 0),
-        'wheel': (0, 127, 0)
-    }
-
     with open(video.name, mode='wb') as f:
-        f.write(video.read()) # save video to disk
+        f.write(video.read()) # save to read by cv2
 
     cap = cv2.VideoCapture(video.name)
     length = cap.get(cv2.CAP_PROP_FRAME_COUNT)
@@ -151,16 +166,24 @@ if video and st.session_state['annotation_result'] is None:
     timestamps = []
     frame_number = 0
 
+    width = int(cap.get(3))
+    height = int(cap.get(4))
+
+    # Heatmap
+    heatmap = np.zeros((int(height), int(width), 3))
+
+    # Draw annotations
     output_path = f'{video.name}.annotated.mp4'
     fourcc = cv2.VideoWriter_fourcc(*'mp4v')
-    width = int(cap.get(3)) 
-    height = int(cap.get(4)) 
-
     out = cv2.VideoWriter(output_path, fourcc, fps, (width,  height))
 
     progress_text = "Drawing annotations. Please wait."
     my_bar = st.progress(0, text=progress_text)
-    st_image = st.empty()
+    frame_img = st.empty()
+    heat_img = st.empty()
+
+    # let's consolidate this later and use tracks below
+    object_annotations = result.annotation_results[0].object_annotations
 
     while(cap.isOpened()):
         ret, img = cap.read()
@@ -174,13 +197,14 @@ if video and st.session_state['annotation_result'] is None:
                     obj_frame = round(f.time_offset.total_seconds() * fps)
                     
                     if abs(frame_number-obj_frame) < 2:
-                        left = int(f.normalized_bounding_box.left * img.shape[1])
-                        right = int(f.normalized_bounding_box.right * img.shape[1])
-                        top = int(f.normalized_bounding_box.top * img.shape[0])
-                        bottom = int(f.normalized_bounding_box.bottom * img.shape[0])
+                        left = int(f.normalized_bounding_box.left * width)
+                        right = int(f.normalized_bounding_box.right * width)
+                        top = int(f.normalized_bounding_box.top * height)
+                        bottom = int(f.normalized_bounding_box.bottom * height)
         
                         if description in colors.keys():
                             color = colors[description]
+                            heatmap[top:bottom,left:right] += color
                         else: 
                             color = (255,255,255)
                             
@@ -189,14 +213,19 @@ if video and st.session_state['annotation_result'] is None:
                         cv2.putText(img, f'Obj #{n}, {description}', (left, bottom),
                                     cv2.FONT_HERSHEY_SIMPLEX, 1, 
                                     color, 2)
-        
+
             timestamps.append(cap.get(cv2.CAP_PROP_POS_MSEC))
             frame_number += 1
 
             out.write(img)
 
             my_bar.progress(frame_number/length, text=progress_text)
-            if frame_number % 10 == 0: st_image.image(img)
+            if frame_number % 10 == 0: 
+                frame_img.image(img)
+                heatmap_norm = np.log(heatmap + 1)
+                heatmap_norm = heatmap_norm/heatmap_norm.max(axis=0).max(axis=0)
+                heat_img.image(heatmap_norm)
+
             # cv2.imshow("annotations", img)
             # if cv2.waitKey(1) == 27:
             #     break
